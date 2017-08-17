1524c483dba703a285a7f260df09e2768b12ae95
JoiceJolly <30997045+JoiceJolly@users.noreply.github.com>
2017-08-17 10:53:45 +0530

    Add files via upload

---
+++ AzureTableStorage/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using Microsoft.WindowsAzure.Storage.Auth;
+using TableStorageAzure1;
+
+namespace AzureTableStorage
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            
+           
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new AzureForm());
+        }
+    }
+}
